5c033d8a11fd9a9764eb33684447d92d4dba0376
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2025-03-17 23:45:31 -0300

    feat: add schemas for task api endpoints

---
+++ backend/app/api/v1/schemas/task.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from app.db.models import TaskBase
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskUpdate(TaskBase):
+    pass
+
+
+class TaskResponse(TaskBase):
+    id: int
+    created_at: datetime
+    modified_at: datetime | None
