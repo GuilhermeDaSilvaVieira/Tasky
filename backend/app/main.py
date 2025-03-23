f08be2cb58001fa547816e6433664ccced1cb900
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2025-03-23 11:16:54 -0300

    feat: include task api router

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.db.session import init_db
+from app.api.v1.endpoints import task
 
 
 @asynccontextmanager
@@ -9,4 +10,6 @@ async def lifespan(app: FastAPI):
     yield init_db()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(title="Tasky API", lifespan=lifespan)
+
+app.include_router(task.router, prefix="/api/v1/tasks", tags=["tasks"])
